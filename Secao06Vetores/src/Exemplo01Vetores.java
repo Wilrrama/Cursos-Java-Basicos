d0482fb49eaa4f72de390f76ce2ac86494e2b83a
wilrrama <wilrrama@hotmail.com>
2021-12-27 21:13:04 -0300

    Secao 06 Vetores Udemy

---
+++ Secao06Vetores/src/Exemplo01Vetores.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exemplo01Vetores {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		double[] vet = new double [n];
+		for(int i=0;i<n;i++) {
+			vet[i]=sc.nextDouble();			
+		}		
+		for(int i=0;i<n;i++) {
+			System.out.printf("%.2f%n", vet[i]);
+		}		
+		sc.close();
+	}
+}
